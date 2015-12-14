33f117247c608b4cf917571dd89d4430c2147bd1
Bryan Hoekstra <hooky@hooky.local>
2015-12-14 14:38:38 +1300

    First push

---
+++ README.md
@@ -0,0 +1,9 @@
+# js-beautify for VS Code 
+
+[![Build Status](https://api.travis-ci.org/HookyQR/VSCodeMinify.svg?branch=master)](https://travis-ci.org/HookyQR/VSCodeMinify)
+
+Minify your js, css and html files to save transmit bandwidth. Runs [minify](http://coderaiser.github.io/minify) on command run (`minify`).
+
+Optionally runs minify on save when a minified file already exists.
+
+Embeded version of minify is v2.0.2.
---
+++ extension.js
@@ -0,0 +1,63 @@
+var vscode = require('vscode');
+var minify = require('minify');
+var path = require('path');
+var fs = require('fs');
+
+//need to register the onSave function here
+
+//register on activation
+function activate(context) {
+    var doMinify = function(doc) {
+        if (doc.isUntitled) return;
+        var better = doc.getText();
+        var type = doc.fileName.split('.').pop().toLowerCase();
+        var run = 'js';
+
+        if (type === 'htm' || type === 'html') run = 'html';
+        else if (type === 'css') run = 'css';
+        else if (type === 'js' || type === 'json') run = 'js';
+        else return;
+        vscode.window.setStatusBarMessage("Starting minify", 5000);
+        minify[run](better, (e, d) => {
+            if (e) {
+                return vscode.window.setStatusBarMessage("Minify failed (convert):" + e.message, 5000);
+            }
+            //we save to the same name with the .min insert
+            var outName = doc.fileName.slice(0, -type.length) + "min." + type;
+            fs.writeFile(outName, d, "utf8", e => {
+                if (e) {
+                    return vscode.window.setStatusBarMessage("Minify failed (output):" + e.message, 5000);
+                    //set a error here?
+                }
+                vscode.window.setStatusBarMessage("Minified: was " + better.length + ", now " + d.length, 5000);
+
+            });
+        });
+    };
+
+    var disposable = vscode.commands.registerCommand('HookyQR.minify', function() {
+        var active = vscode.window.activeTextEditor;
+        if (active) {
+            var doc = active.document;
+            if (doc) {
+                return doMinify(doc);
+            }
+        }
+        vscode.window.setStatusBarMessage("File must be saved before minify can run", 5000);
+
+    });
+    context.subscriptions.push(disposable);
+
+    vscode.workspace.onDidSaveTextDocument(function (doc) {
+		//check if the user wants to do a minify here
+		if (!vscode.workspace.getConfiguration('minify')['minifyExistingOnSave']) return;
+        //check if there is a *.min.* file
+        var n = doc.fileName.split('.');
+        var ext = "min." + n.pop();
+        n.push(ext);
+        n = n.join(".");
+        //see if there is a file, if there is, run min
+        if (fs.existsSync(n)) doMinify(doc);
+    });
+}
+exports.activate = activate;
